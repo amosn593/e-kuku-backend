a60b02dc45aa4c000ae051acbf013a2322107760
Amos Ndonga <amosndonga@gmail.com>
2021-07-26 12:12:11 +0300

    added serializers.py content

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,2 +1,23 @@
 from rest_framework import serializers
 from .models import *
+
+
+class CountySerializer(serializers.Serializer):
+    class Meta:
+        model: County
+        Fields: '__all__'
+        
+class SubcountySerializer(serializers.Serializer):
+    class Meta:
+        model: Subcounty
+        Fields: '__all__'
+        
+class CategorySerializer(serializers.Serializer):
+    class Meta:
+        model: Category
+        Fields: '__all__'
+        
+class PoultrySerializer(serializers.Serializer):
+    class Meta:
+        model: Poultry
+        Fields: '__all__'
