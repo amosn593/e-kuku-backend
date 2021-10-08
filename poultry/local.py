f86bd8fdc10a1a01cec18060e090b45f37048881
Amos Ndonga <amosndonga@gmail.com>
2021-10-08 11:31:11 +0300

    run collect statics

--- poultry/local.py
+++ poultry/local.py
@@ -192,7 +192,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_local')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
