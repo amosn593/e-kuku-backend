65f5ff1b53caa03486c7141b284e0beda3479e91
Amos Ndonga <amosndonga@gmail.com>
2021-10-30 21:22:36 +0300

    refactored sell.js

--- mpesa/urls.py
+++ mpesa/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('lipa/', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
 
     # register, confirmation, validation and callback urls
-    path('api/v1/c2b/register', views.register_urls,
-         name="register_mpesa_validation"),
-    path('c2b/confirmation', views.confirmation, name="confirmation"),
-    path('c2b/validation', views.validation, name="validation"),
-    path('c2b/callback', views.call_back, name="call_back"),
+    # path('api/v1/c2b/register', views.register_urls,
+    #      name="register_mpesa_validation"),
+    # path('c2b/confirmation', views.confirmation, name="confirmation"),
+    # path('c2b/validation', views.validation, name="validation"),
+    # path('c2b/callback', views.call_back, name="call_back"),
 
 ]
